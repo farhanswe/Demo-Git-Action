fc01a4a56290d43f47d69934cf9d67db279ffe76
Farhan Tanvir <53516648+farhanswe@users.noreply.github.com>
2024-07-06 12:31:04 +0600

    Update DemoController.java

--- src/main/java/com/farhantanvir/demogitaction/controllers/DemoController.java
+++ src/main/java/com/farhantanvir/demogitaction/controllers/DemoController.java
@@ -11,8 +11,8 @@ public class DemoController {
     @GetMapping
     public String getDemo(){
         System.out.println("Hello");
-        System.out.println("Hello Demo GIT ACTION V6");
-        return "Hello Demo GIT ACTION V6";
+        System.out.println("Hello Demo GIT ACTION V6 Hello");
+        return "Hello Demo GIT ACTION V6 Hello";
     }
 
 }
