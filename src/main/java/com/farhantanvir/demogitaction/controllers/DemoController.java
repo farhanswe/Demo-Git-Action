dd58c538c593ff01ae594e8c8a23bd7abdf1e5b1
Farhan Tanvir <farhanswe35@gmail.com>
2024-07-06 12:41:54 +0600

    Update

--- src/main/java/com/farhantanvir/demogitaction/controllers/DemoController.java
+++ src/main/java/com/farhantanvir/demogitaction/controllers/DemoController.java
@@ -11,8 +11,8 @@ public class DemoController {
     @GetMapping
     public String getDemo(){
         System.out.println("Hello");
-        System.out.println("Hello Demo GIT ACTION V6 Branch Farhan");
-        return "Hello Demo GIT ACTION V6 Branch Farhan";
+        System.out.println("Hello Demo GIT ACTION V6");
+        return "Hello Demo GIT ACTION V6";
     }
 
 }
